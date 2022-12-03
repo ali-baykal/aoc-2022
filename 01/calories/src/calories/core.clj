d5ade9c07e357f336540b49c60f56d4269f74da2
Ali Baykal <alib@dasburo.com>
2022-12-03 18:36:06 +0100

    01_1: solve

--- 01/calories/src/calories/core.clj
+++ 01/calories/src/calories/core.clj
@@ -1,7 +1,22 @@
 (ns calories.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(def input-path (.getPath (clojure.java.io/resource "input")))
+(def input-content (slurp input-path))
+
+(defn map2 [mapping-func two-dim] 
+  (map (fn [group] (map mapping-func group)) two-dim))
+
+(defn group-calories [input]
+  (map2 #(Integer/parseInt %) (map (fn [calories-str] (str/split calories-str #"\n")) (str/split input #"\n\n"))))
+
+(defn sum [x] (reduce + x))
+
+(defn sum-calories [grouped-calories]
+  (map sum grouped-calories))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (apply max (sum-calories (group-calories input-content)))))
--- 01/calories/test/calories/core_test.clj
+++ 01/calories/test/calories/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [calories.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def input "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
+
+
+(deftest group-calories-test
+  (testing "it parses input in a two dimensinal list of numbers"
+    (is (= (group-calories input) [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]))))
