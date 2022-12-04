1b67b1371a9c143ae1e997c16fde698800142479
Ali Baykal <alib@dasburo.com>
2022-12-04 13:15:32 +0100

    04_1: solve

--- 04/camp_cleanup/src/camp_cleanup/core.clj
+++ 04/camp_cleanup/src/camp_cleanup/core.clj
@@ -1,7 +1,34 @@
 (ns camp-cleanup.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(def input-path (.getPath (clojure.java.io/resource "input")))
+(def input-content (slurp input-path))
+
+(defn split-range [range]
+  (map #(Integer/parseInt %) (str/split range #"-")))
+
+(defn parse-input [input]
+  (map #(map split-range (str/split % #",")) (str/split-lines input-content)))
+
+
+(defn fully-contains? [larger-range, smaller-range]
+  (let [ 
+    larger-range-start (first larger-range)
+    larger-range-end (last larger-range)
+    smaller-range-start (first smaller-range)
+    smaller-range-end (last smaller-range)
+  ]
+    (and (>= smaller-range-start larger-range-start) (<= smaller-range-end larger-range-end)))
+)
+
+(defn overlaps? [pair]
+  (or (apply fully-contains? pair) (apply fully-contains? (reverse pair))))
+
+(defn find-overlaps [pair-list]
+  (filter overlaps? pair-list))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (count (find-overlaps (parse-input input-content)))))
--- 04/camp_cleanup/test/camp_cleanup/core_test.clj
+++ 04/camp_cleanup/test/camp_cleanup/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [camp-cleanup.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest fully-contains-test
+  (testing "checks if the first range fully contains the second one"
+    (is (= (fully-contains? '(1 7) '(3 4)) true))
+    (is (= (fully-contains? '(1 3) '(3 4)) false))
+    (is (= (fully-contains? '(3 4) '(1 7)) false))
+  ))
+
+(deftest overlaps-test
+  (testing "checks if the any of ranges (within a pair) fully contains the other"
+    (is (= (overlaps? '([1 7] [3 4])) true))
+    (is (= (overlaps? '([1 3] [3 4])) false))
+    (is (= (overlaps? '([3 4] [1 7])) true))
+  ))
