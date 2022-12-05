ae236793b6c34668cd92b82ee9ac75511094507c
Ali Baykal <alib@dasburo.com>
2022-12-05 21:52:20 +0100

    05_1: create the project

---
+++ 05/crates/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crates
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ 05/crates/test/crates/core_test.clj
@@ -0,0 +1,7 @@
+(ns crates.core-test
+  (:require [clojure.test :refer :all]
+            [crates.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
